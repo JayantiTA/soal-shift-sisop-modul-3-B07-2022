f745bfd14ffe66528c99644690fbb0b27ff7e712
JayantiTA <jayjayanti18@gmail.com>
2022-04-08 03:47:23 +0700

    feat: add soal1 solution (until 1d)

--- soal1/soal1.c
+++ soal1/soal1.c
@@ -12,7 +12,10 @@ const char *link2 = "https://docs.google.com/uc?export=download&id=1jR67_JAyozZP
 
 void* download_and_unzip(char* folder_name);
 void* decode_base64(char* folder_name, char* file_name);
-void* move_to_folder(char* filename);
+void *create_no_txt();
+void *unzip_file_using_password(char* file_name);
+void *zip_hasil_folder();
+void* move_to_folder(char* folder_name, char* file_name);
 
 void *print_message_function( void *ptr );
 
@@ -40,10 +43,79 @@ int main()
     // printf("pthread_create() for thread 1 returns: %d\n",iret1);
     // printf("pthread_create() for thread 2 returns: %d\n",iret2);
 
-    pthread_join( thread1, NULL);
-    pthread_join( thread2, NULL); 
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL); 
+
+    zip_hasil_folder();
+
+    printf("halo");
+
+    pthread_t thread3, thread4;//inisialisasi awal
+    int  iret3, iret4;
+
+    iret3 = pthread_create( &thread3, NULL, unzip_file_using_password, "hasil.zip"); //membuat thread pertama
+    if (iret3)
+    {
+        fprintf(stderr,"Error - pthread_create() return code: %d\n",iret1);
+        exit(EXIT_FAILURE);
+    }
+
+    // iret2 = pthread_create( &thread2, NULL, create_no_txt, NULL);//membuat thread kedua
+    // if (iret2)
+    // {
+    //     fprintf(stderr,"Error - pthread_create() return code: %d\n",iret2);
+    //     exit(EXIT_FAILURE);
+    // }
 
     exit(EXIT_SUCCESS);
+}
+
+void *zip_hasil_folder() {
+  pid_t child_id_1, child_id_2;
+  int status1;
+
+  if (child_id_1 == 0) {
+    int status2;
+    child_id_2 = fork();
+
+    if (child_id_2 == 0) {
+      char* argv[] = {"zip", "-r", "-P", "mihinomenestjay", "hasil.zip", "hasil", NULL};
+      execv("/usr/bin/zip", argv);
+    } else {
+      while ((wait(&status2)) > 0);
+      char *argv[] = {"rm", "-rf", "hasil", NULL};
+      printf("masuk");
+      execv("/bin/rm", argv);
+    }
+  } else {
+    while ((wait(&status1)) > 0);
+  }
+}
+
+void *unzip_file_using_password(char* file_name) {
+  pid_t child_id_1;
+  int status1;
+  if (child_id_1 == 0) {
+    char* argv[] = {"unzip", "-P", "mihinomenestjay", "-d", file_name, NULL};
+    execv("/usr/bin/unzip", argv);
+  } else {
+    while ((wait(&status1)) > 0);
+  }
+}
+
+void *create_no_txt() {
+  pid_t child_id_1;
+  int status1;
+  if (child_id_1 == 0) {
+    char* argv[] = {"touch", "no.txt", NULL};
+    execv("/usr/bin/touch", argv);
+  } else {
+    while ((wait(&status1)) > 0);
+    FILE *file_no_txt;
+    file_no_txt = fopen("no.txt", "w+");
+    fprintf(file_no_txt, "No");
+    fclose(file_no_txt);
+  }
 }
 
 void *print_message_function( void *ptr )
@@ -59,7 +131,7 @@ void *print_message_function( void *ptr )
 
 void* download_and_unzip(char* folder_name)
 {
-  pid_t child_id_1, child_id_2, child_id_3;
+  pid_t child_id_1, child_id_2, child_id_3, child_id_4, child_id_5;
   child_id_1 = fork();
   int status1;
 
@@ -86,22 +158,41 @@ void* download_and_unzip(char* folder_name)
     int status3;
 
     if (child_id_3 == 0) {
-      char *file_name = malloc(sizeof(char) * 100);
-      strcpy(file_name, folder_name);
-      strcat(file_name, ".zip");
-      char *argv[] = {"rm", file_name};
-      execv("/usr/bin/rm", argv);
+      child_id_4 = fork();
+      int status4;
+      if (child_id_4 == 0) {
+        char *file_name = malloc(sizeof(char) * 100);
+        strcpy(file_name, folder_name);
+        strcat(file_name, ".zip");
+        char *argv[] = {"rm", file_name};
+        execv("/usr/bin/rm", argv);
+      } else {
+        while((wait(&status4)) > 0);
+        char *file_name = malloc(sizeof(char) * 100);
+        strcpy(file_name, folder_name);
+        strcat(file_name, ".txt");
+        char *argv[] = {"touch", file_name, NULL};
+        execv("/usr/bin/touch", argv);
+      }
     } else {
       while ((wait(&status3)) > 0);
-      // printf("success");
-      DIR *dir_path;
-      struct dirent *dir;
-      dir_path = opendir(folder_name);
-      while((dir = readdir(dir_path))) {
-        if (strcmp(dir->d_name, ".") != 0 && strcmp(dir->d_name, "..") != 0) {
-          decode_base64(folder_name, dir->d_name);
+      child_id_5 = fork();
+      int status5;
+
+      if (child_id_5 == 0) {
+        DIR *dir_path;
+        struct dirent *dir;
+        dir_path = opendir(folder_name);
+        while((dir = readdir(dir_path))) {
+          if (strcmp(dir->d_name, ".") != 0 && strcmp(dir->d_name, "..") != 0) {
+            decode_base64(folder_name, dir->d_name);
+          }
         }
+      } else {
+        while ((wait(&status5)) > 0);
+        move_to_folder(folder_name, folder_name); //memindahkan file ke folder
       }
+      // printf("success");
     }
   }
 }
@@ -113,24 +204,36 @@ void* decode_base64(char* folder_name, char* file_name)
   strcat(filepath, "/");
   strcat(filepath, file_name);
 
+  char file_txt[100];
+  strcpy(file_txt, folder_name);
+  strcat(file_txt, ".txt");
+
   FILE *file;
   file = fopen(filepath, "a+");
+
+  FILE *file_base64;
+  file_base64 = fopen(file_txt, "a");
+
   char str[255];
   char decoded[255];
   // fprintf(file, "%s\n", file_name);
   // printf("%s\n", filepath);
-  while(fgets(str, 255, file))
+  while(fgets(str, 255, file)) {
     printf("%s\n", str);
+    // base64_decode(str, decoded);
+    fprintf(file_base64, "%s\n", str);
+  }
+    // printf("%s\n", str);
   // freopen("")
 
-  pid_t child_id_1;
-  if (child_id_1 == 0) {
-    char *argv[] = {"base64", "-d", filepath, NULL};
-    execv("/usr/bin/base64", argv);
-  } else {
-    while ((wait(NULL)) > 0);
-    // move_to_folder(file_name);
-  }
+  // pid_t child_id_1;
+  // if (child_id_1 == 0) {
+  //   char *argv[] = {"base64", "-d", filepath, NULL};
+  //   execv("/usr/bin/base64", argv);
+  // } else {
+  //   while ((wait(NULL)) > 0);
+  // }
+  // move_to_folder(folder_name, file_name);
   // while(fgets(str, 255, file)) {
   //   char *argv[] = {"base64", "-d", file_name, NULL};
   // }
@@ -139,4 +242,47 @@ void* decode_base64(char* folder_name, char* file_name)
   
   // puts("");
   fclose(file);
+}
+
+void* move_to_folder(char* folder_name, char* file_name)
+{
+  char filepath[100];
+  strcpy(filepath, folder_name);
+  strcat(filepath, "/");
+  strcat(filepath, file_name);
+
+  pid_t child_id_1, child_id_2;
+
+  child_id_1 = fork();
+  int status1;
+  if (child_id_1 == 0) {
+    child_id_2 = fork();
+    int status2;
+
+    if (child_id_2 == 0) {
+      char *argv[] = {"rm", "-rf", folder_name, NULL};
+      execv("/usr/bin/rm", argv);
+    } else {
+      while ((wait(&status2)) > 0);
+      char *argv[] = {"mkdir", "-p", "hasil", NULL};
+      execv("/usr/bin/mkdir", argv);
+    }
+  } else {
+    while ((wait(&status1)) > 0);
+    pid_t child_id_3 = fork();
+    int status3;
+
+    if (child_id_3 == 0) {
+      char filename[100];
+      strcpy(filename, folder_name);
+      strcat(filename, ".txt");
+      char *argv[] = {"mv", filename, "hasil", NULL};
+      execv("/usr/bin/mv", argv);
+    } else {
+      while ((wait(&status3)) > 0);
+    }
+
+    // char *argv[] = {"rm", "-rf", filepath, NULL};
+    // execv("/usr/bin/rm", argv);
+  }
 }
